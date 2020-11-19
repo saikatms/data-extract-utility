64cb3d778c6a419f6649468ed94c43dc29164363
Saikat Mondal <saikatm1997@gmail.com>
2020-11-19 16:13:22 +0530

    OCR upgreaded

--- OCR/src/Run.php
+++ OCR/src/Run.php
@@ -1,26 +1,21 @@
 <?php
-
 namespace OCR\PdfToImage;
 
 use OCR\TesseractOCR\TesseractOCR;
-
-// use OCR\TesseractOCR\TesseractOCR;
-
 include_once __DIR__."/Pdf.php";
 include_once __DIR__."/TesseractOCR.php";
-$filename="/home/saikat/Downloads/cc.txt";
+
+$filename="/home/saikat/Downloads/cc.txt"; //Text file name 
 $handle=fopen($filename, "w+");
-$pathToPdf="/home/saikat/Downloads/mmm/mmm.pdf";
+
+$pathToPdf="/home/saikat/Downloads/mmm/mmm.pdf"; //Pdf Path
 $pdf = new Pdf($pathToPdf);
 $ocr=new TesseractOCR();
-$pathToWhereImageShouldBeStored="/home/saikat/Downloads/";
-// $pdf->saveImage($pathToWhereImageShouldBeStored);
+$imagepath="/home/saikat/Downloads/Out/"; //Image Loc
 $pageCount=$pdf->getNumberOfPages();
-// for($i=1;$i<$pageCount;$i++){
-    // $pdf->setPage($i)->saveImage("/home/saikat/Downloads/Out/page_".$i.".jpg");
-    $ocr->image("/home/saikat/Downloads/Out/page_242.jpg"    );
-    $data=$ocr->lang("ben")->run();
+for($i=1;$i<$pageCount;$i++){
+    $pdf->setPage($i)->saveImage($imagepath."/page_".$i.".jpg");
+    $ocr->image($imagepath."/page_".$i.".jpg");
+    $data=$ocr->lang("ben","eng")->run();    
     fwrite($handle, $data.PHP_EOL);
-    print_r($data);
-    exit();
-// }
+}
